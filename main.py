af2b149f0988e0d545efc9d8cb706ae105b6463f
Majid Shajari <shajari.py@gmail.com>
2025-09-25 22:09:31 +0330

    update autication and enroll

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 
-from src.config import LED
+from src.config import LED,ENCRYPTED_PATH,DECRYPTED_PATH
 from src.core.enroll import Enroll
 from src.core.identify import Identify
+from src.security.encrypt import Encrypt
 from src.core.status import SensorStatus
 from src.utils.setup_paths import ensure_paths
 
@@ -39,9 +40,17 @@ else:
     time.sleep(2)
     raise SystemExit("Enrollment Failed")
 enroller.close()
+encrypter = Encrypt()
+file_path = ENCRYPTED_PATH/"4433509132.bin"
+encrypted_data = encrypter.encrypt_to_file(file_path,data,"11235813")
+if not encrypted_data:
+    raise SystemExit("Encryption Failed")
+decrypted_data = encrypter.decrypt_from_file(file_path,"11235813")
+if not decrypted_data:
+    raise SystemExit("Decryption Failed")
 identifier = Identify(on_status=feedback)
 identifier.config_led(LED["ready"])
-status = identifier.upload_to_sensor(data)
+status = identifier.upload_to_sensor(decrypted_data)
 if status[0] == SensorStatus.SUCCESS:
     print(f"Uploaded successfully in :{status[1]}")
     identifier.config_led(LED["succes"])
--- src/security/encrypt.py
+++ src/security/encrypt.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
+from src.utils.logger import setup_logger
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
+local_logger = setup_logger("Encrypt")
 
 class Encrypt:
     def __init__(self, iterations: int = 390000):
@@ -22,7 +23,7 @@ class Encrypt:
         )
         return kdf.derive(password.encode())
 
-    def encrypt(self, data: bytes, password: str) -> tuple[bytes, bytes, bytes]:
+    def _encrypt(self, data: bytes, password: str) -> tuple[bytes, bytes, bytes]:
         if not isinstance(data, bytes):
             data = bytes(data)
         salt = os.urandom(16)
@@ -32,18 +33,22 @@ class Encrypt:
         ct = aesgcm.encrypt(nonce, data, None)
         return ct, nonce, salt
 
-    def decrypt(self, ct: bytes, nonce: bytes, salt: bytes, password: str) -> bytes:
+    def _decrypt(self, ct: bytes, nonce: bytes, salt: bytes, password: str) -> bytes:
         key = self._derive_key(password, salt)
         aesgcm = AESGCM(key)
-        return aesgcm.decrypt(nonce, ct, None)
+        local_logger.info("Decrypting data...")
+        return list(aesgcm.decrypt(nonce, ct, None))
 
     def encrypt_to_file(self, filepath: Path, data: bytes, password: str):
-        ct, nonce, salt = self.encrypt(data, password)
+        ct, nonce, salt = self._encrypt(data, password)
         with open(filepath, "wb") as f:
             f.write(salt + nonce + ct)
+        local_logger.info(f"Data encrypted and saved to {filepath}")
+        return True
 
     def decrypt_from_file(self, filepath: Path, password: str) -> bytes:
         with open(filepath, "rb") as f:
             raw = f.read()
         salt, nonce, ct = raw[:16], raw[16:28], raw[28:]
-        return self.decrypt(ct, nonce, salt, password)
+        local_logger.info(f"Data read from {filepath}, starting decryption")
+        return self._decrypt(ct, nonce, salt, password)
